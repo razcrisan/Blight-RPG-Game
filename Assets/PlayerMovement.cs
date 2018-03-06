165774d8655c782d4e2bb5f4d24f6765345688e5
Razvan Crisan <rcrisan13@gmail.com>
2018-03-06 10:07:35 -0500

    added gamepad movement and click to move alternative

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,12 +5,14 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
-	
+
 	[SerializeField] float walkMoveStopRadius = 0.1f;
 
 	ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
 	CameraRaycaster cameraRaycaster;
 	Vector3 currentClickTarget;
+
+	bool isInDirectMode = false; // TODO make static later
 
 	private void Start()
 	{
@@ -23,6 +25,30 @@ public class PlayerMovement : MonoBehaviour
 
 	// Fixed update is called in sync with physics
 	private void FixedUpdate()
+	{
+		if (Input.GetKeyDown (KeyCode.G)) // TODO allow player to switch to gamepad mode
+		{ 
+			isInDirectMode = !isInDirectMode; //TODO add to menu later
+		}
+
+		if (isInDirectMode)
+		{
+			ProcessDirectMovement();
+		}
+		else
+		{
+		ProcessMouseMovement(); //processes mouse movement
+		}
+	}
+
+	private void ProcessDirectMovement();
+	{
+	
+	}
+
+
+
+	private void ProcessMouseMovement()
 	{
 		if (Input.GetMouseButton(0))
 		{
